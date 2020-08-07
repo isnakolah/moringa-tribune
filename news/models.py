db9a0482ef1d96ef54a474ec3ae496c5e02e5909
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-08-07 11:52:56 +0300

    class Meta of ordering the Editors in reverse order

--- news/models.py
+++ news/models.py
@@ -5,3 +5,9 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['-first_name']
