60767ed935d17205a90f37570a190565cd20e6ac
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-08-07 09:55:16 +0300

    block.super in template to inherit child elements of the block

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 
 
@@ -7,43 +7,27 @@ def welcome(request):
     return render(request, 'news/welcome.html')
 
 
-def convert_dates(dates):
-    # Function that gets the weekday number for the date
-    day_number = datetime.date.weekday(dates)
-
-    days = ['Monday', 'Tuesday', 'Wednesday',
-            'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
-
 def past_days_news(request, past_date):
+
     try:
         date = datetime.datetime.strptime(past_date, '%Y-%m-%d').date()
-        html = f'''
-               <html>
-                   <body>News for day {date.day}-{date.month}-{date.year}</body>
-               </html>
-               '''
+
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-    return HttpResponse(html)
+        assert False
+
+    if date == datetime.date.today():
+        return redirect(news_of_the_day)
+
+    return render(request, 'news/past-news.html', {
+        'date': date,
+    })
 
 
 def news_of_the_day(request):
     date = datetime.date.today()
 
-    # Function to convert date object to find exact day
-    day = convert_dates(date)
-
-    html = f'''
-        <html>
-            <body>
-                <h1>{date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-        '''
-    return HttpResponse(html)
+    return render(request, 'news/today', {
+        'date': date,
+    })
