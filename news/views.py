5fb6fab292e7334aa3ccbacb92f9d74e161f7ce2
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-08-09 05:47:19 +0300

    fix subscribe to newsletter

--- news/views.py
+++ news/views.py
@@ -13,7 +13,11 @@ def news_today(request):
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name=name, email=email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
         else:
             form = NewsLetterForm()
 
